ae4aefaaea3c7ccdb76d25852ffb016a1f00bf5e
Afkar Mohamad <femyhotforex@gmsail.com>
2026-02-04 03:13:14 +0000

    auto-commit for 361f2722-7236-4c93-9004-2abc9f791b2b

--- backend_test.py
+++ backend_test.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import io
 
 class AgroMopumuloAPITester:
-    def __init__(self, base_url="https://agro-mopomulo.preview.emergentagent.com"):
+    def __init__(self, base_url="https://agro-dashboard-6.preview.emergentagent.com"):
         self.base_url = base_url
         self.token = None
         self.tests_run = 0
